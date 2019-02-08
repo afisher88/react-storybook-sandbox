f927e8a80ebc7f8449d9d5d17b7627ab4bfe92ba
Anthony Fisher <Anthony.Fisher@motors.internal>
2019-02-08 16:51:53 +0000

    Initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure, addDecorator } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /.stories.js$/);
+
+addDecorator(checkA11y);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
